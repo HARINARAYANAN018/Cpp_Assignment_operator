171722d115861424a0919b81607c89850eedca10
HARINARAYANAN018 <79526544+HARINARAYANAN018@users.noreply.github.com>
2022-04-16 20:56:40 +0530

    Assignmentoprt

---
+++ Assignmentoprt.cpp
@@ -0,0 +1,12 @@
+//2 program: using assignment operator to find the product of two float number
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a , b;
+	cout<<"Enter the first value: ";
+	cin>>a;
+	cout<<"Enter the second value: ";
+	cin>>b;
+	cout<<"The product of two balue is:"<<a*b;
+}
